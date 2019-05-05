9a2bf9b9aa74735dab190b03776fb2811d943058
Nishant Desai <nishantdesai1306@gmail.com>
2019-05-05 18:17:30 +0530

    removed package lock

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,8 +1,8 @@
 export default {
-  apiKey: 'AIzaSyCIVCFFmeJ6OjfwAmMRBq3s9H48lO8jpOo',
-  authDomain: 'smartcopy-195fd.firebaseapp.com',
-  databaseURL: 'https://smartcopy-195fd.firebaseio.com',
-  projectId: 'smartcopy-195fd',
-  storageBucket: 'smartcopy-195fd.appspot.com',
-  messagingSenderId: '961123670467',
+	apiKey: 'AIzaSyCIVCFFmeJ6OjfwAmMRBq3s9H48lO8jpOo',
+	authDomain: 'smartcopy-195fd.firebaseapp.com',
+	databaseURL: 'https://smartcopy-195fd.firebaseio.com',
+	projectId: 'smartcopy-195fd',
+	storageBucket: 'smartcopy-195fd.appspot.com',
+	messagingSenderId: '961123670467',
 };
