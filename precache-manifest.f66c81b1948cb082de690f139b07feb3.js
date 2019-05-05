a8263b8d195fea67236352f5b5369707dab3756e
Nishant Desai <nishantdesai1306@gmail.com>
2019-05-05 18:50:57 +0530

    Updates

---
+++ precache-manifest.f66c81b1948cb082de690f139b07feb3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "541f7a3c0704491e5a24c2624d3bca00",
+    "url": "/smart-copy-web-firebase/index.html"
+  },
+  {
+    "revision": "7ba4893a4d870644a284",
+    "url": "/smart-copy-web-firebase/static/js/2.0542ae16.chunk.js"
+  },
+  {
+    "revision": "77bfb46f884ef61956e4",
+    "url": "/smart-copy-web-firebase/static/js/main.ece34714.chunk.js"
+  },
+  {
+    "revision": "e4b330ec6eb768965865",
+    "url": "/smart-copy-web-firebase/static/js/runtime~main.f4bda059.js"
+  }
+]);
